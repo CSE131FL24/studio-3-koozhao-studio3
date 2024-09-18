d7c6973b8d3231c595298c1f00bde2c3972c3f85
billyzhao <billyzhao@localhost>
2024-09-18 15:13:04 -0500

    completed studio again

--- src/studio3/Sieve.java
+++ src/studio3/Sieve.java
@@ -17,7 +17,7 @@ public class Sieve {
 		for (int i=2; i <= n; i++) {
 			if (prime [i-1]) {
 				System.out.println(i);
-			for (int j=i*i; j <= n; j+=i) {
+			for (int j=i; j <= n; j+=i) {
 					prime [j-1] = false;
 					
 				}
